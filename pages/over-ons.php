254ab694de486ce8de179bef56426d490e977054
Merel Kooistra <rv670279@edu.rocrivor.nl>
2025-11-12 10:14:27 +0100

    pages upddate bruh

---
+++ pages/over-ons.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1, initial-scale=1.0">
+    <link rel="stylesheet" href="css/over-ons.css">
+    <title>meerkaarts.io</title>
+</head>
+<body>
+    <ul> 
+     <li><a href="home-page.php">Home</a></li>
+     <li><a href="#news">News</a></li>
+     <li><a href="#contact">Contact</a></li>
+     <li><a href="#about">over ons</a></li>
+    </ul>
+    <h1>Over ons</h1>
+    reden: <br>
+    omdat onze lieve grappige docent weg gaat zonder reden wtf???!!!!!???<br>
+    <br>
+    wie hebben dit  gemaakt: <br>
+    <ul class = " kraats">
+        <li>Sophie</li> <br>
+        <li>Sam</li> <br>
+        <li>Bram</li> <br>
+        <li>Brett</li> <br>
+        <li>Morten</li> <br>
+        <li>Fedde</li> <br>
+        <li>Inge</li> <br>
+        <li>Merel</li> <br>
+    </ul>
+</body>
+</html>

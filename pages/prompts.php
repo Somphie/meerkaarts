fb5531173435b576067d2ffff9f2d011467090a0
laniakea-rv <rv669647@edu.rocrivor.nl>
2025-11-13 09:15:17 +0100

    nieuwe pagina toegevoegd

---
+++ pages/prompts.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>gamemodes</title>
+  <link rel="stylesheet" href="css/prompts.css"/>
+</head>
+<body>
+
+  <nav class="navbar">
+  <ul>
+    <li><a href="#">over ons</a></li>
+    <li><a href="#">gamemodes</a></li>
+    <li><a href="#">home</a></li>
+    <li><a href="#" class="selected">prompts inzenden</a></li>
+    <li><a href="#">samballen</a></li>
+  </ul>
+</nav>
+
+  <main class="content">
+    <h1 class="page-title">Prompts inzenden</h1>
+
+    <div class="prompt-card">
+      <div class="prompt-header">stuur een promt</div>
+      <input class="prompt-input" type="text" placeholder="insert promt" />
+      <button class="prompt-button">stuur op</button>
+    </div>
+  </main>
+</body>
+</html>

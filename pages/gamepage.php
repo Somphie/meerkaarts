38256d21a3ae92b1eb3398f09859661962b06754
Sophie kicken <rv669457@edu.rocrivor.nl>
2025-11-18 15:25:48 +0100

    gamepage gefixed

--- pages/gamepage.php
+++ pages/gamepage.php
@@ -12,21 +12,13 @@
     <?php include("../presets/nav.php"); ?>
     <main>
         <div class="grootediv">
-<<<<<<< HEAD
+
             <div class="blauw">
                 <h1>De GAme</h1>
 
                 <div class="speelvlak"></div>
             </div>
         </div>
-=======
-        <div class="blauw">
-            <h1>De GAme</h1>
-            <div class="speelvlak"></div>
-        </div>
-        
-        </div>
->>>>>>> e4594c32dce2439b0169906691e29092fa8b467a
     </main>
 </body>
 

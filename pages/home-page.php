5cd54e18e9c029f449c0fd818476bebf106dc46b
Merel Kooistra <rv670279@edu.rocrivor.nl>
2025-11-18 14:25:48 +0100

    homepage

--- pages/home-page.php
+++ pages/home-page.php
@@ -9,27 +9,13 @@
 </head>
 
 <body>
-<<<<<<< HEAD
-     <nav>
-        <ul>
-            <li><a href="over-ons.php">Over ons</a></li>
-            <li><a href="gamepage.php">Gamemodes</a></li>
-            <li><a href="home-page.php">Home</a></li>
-            <li><a href="">Prompts inzenden</a></li>
-            <li><a href="">de samballen</a></li>
-        </ul>
-</nav>
-    <div class="boomboom">
-        <img src="minesweeper.jpg" alt="minesweeper">
-    </div>
-=======
     <?php include("../presets/nav.php"); ?>
+    
     <main>
         <div class="Img">
             <img src="images/minesweeper.jpg" alt="minesweeper">
         </div>
     </main>
->>>>>>> e9fcefa387919df70b245e161630334062a3a134
 </body>
 
 </html>

e36b6a645acada69ecc474d1b763dfe608e6943c
laniakea-rv <rv669647@edu.rocrivor.nl>
2025-11-25 15:14:48 +0100

    databases geupdatet

--- database/prompts_database.php
+++ database/prompts_database.php
@@ -1,12 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "database naam";
+$servername = "database-5019078569.webspace-host.com";
+$username = "dbu1219007";
+$password = "desamballen2025RAHHHH";
+$dbname = "dbs15004602";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>

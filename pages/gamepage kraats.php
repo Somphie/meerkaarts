fda8ec4d2b7693c513b984e0353b851e16938d6c
Sophie kicken <rv669457@edu.rocrivor.nl>
2025-11-25 14:20:27 +0100

    kraats gamepage gemaakt

---
+++ pages/gamepage kraats.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/gamepage kraats.css">
+    <title>meerkaarts.io</title>
+</head>
+
+<body>
+    <?php include("../presets/nav.php"); ?>
+    <main>
+        <div class="alles">
+
+            <div class="partyblock">
+                <h1>PArty</h1>
+                <div class="uservlak"></div>
+            </div>
+
+            <div class="grootediv">
+
+                <div class="blauw">
+                    <h1>De GAme</h1>
+
+                    <div class="speelvlak">
+                        <div class="spelletje"></div>
+                    </div>
+                </div>
+            </div>
+                <div class="rechts">
+                    <h1>Sabotage</h1>
+                    <div class="sabotages"></div>
+                </div>
+        </div>
+    </main>
+</body>
+
+</html>
